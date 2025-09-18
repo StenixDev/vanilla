5c7a116cb88a3a3a4e30c0edebe25b229ad873a2
StenixDev <stenix.dev@gmail.com>
2025-09-19 05:37:09 +0800

    Using Composition to make a Reusable Box

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ const average = (arr) =>
 
 export default function App() { 
   const [movies, setMovies] = useState(tempMovieData);
+   const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>       
@@ -62,10 +63,13 @@ export default function App() {
       </NavBar>
       <Main>
         
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-          </ListBox>
-       <WatchedBox />
+          </Box>
+      <Box>
+        <WatchedSummary watched={watched} />
+        <WatchedMovieList watched={watched} />
+      </Box>
         </Main> 
     </>
   );
@@ -112,20 +116,20 @@ function Main({children}){
 }
 
 
-function ListBox({children}){
+function Box({children}){
 
   
 
-  const [isOpen1, setIsOpen1] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
 
   return <div className="box">
           <button
             className="btn-toggle"
-            onClick={() => setIsOpen1((open) => !open)}
+            onClick={() => setIsOpen((open) => !open)}
           >
-            {isOpen1 ? "–" : "+"}
+            {isOpen ? "–" : "+"}
           </button>
-          {isOpen1 && children}
+          {isOpen && children}
         </div>
 }
 
@@ -152,36 +156,6 @@ function Movie({movie}){
                     </p>
                   </div>
                 </li>
-}
-
-
-function WatchedBox(){
-
-    const [watched, setWatched] = useState(tempWatchedData);
-  
-  const [isOpen2, setIsOpen2] = useState(true);
-
- 
-
-  return <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen2((open) => !open)}
-          >
-            {isOpen2 ? "–" : "+"}
-          </button>
-          {isOpen2 && (
-            <>
-
-            <WatchedSummary watched={watched} />
-
-            <WatchedMovieList watched={watched} />
-             
-
-              
-            </>
-          )}
-        </div>
 }
 
 
