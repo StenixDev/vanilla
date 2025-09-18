6a9bfb40c2ea3f3f197a5b5d113d12431ce8ea23
StenixDev <stenix.dev@gmail.com>
2025-09-18 20:04:23 +0800

    Prop Drilling

--- src/App.js
+++ src/App.js
@@ -51,24 +51,24 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() { 
-  
+  const [movies, setMovies] = useState(tempMovieData);
 
   return (
     <>       
-      <NavBar />
-      <Main />      
+      <NavBar movies={movies} />
+      <Main movies={movies} />      
     </>
   );
 }
 
 
-function NavBar(){
+function NavBar({movies}){
 
 
   return <nav className="nav-bar">
         <Logo />
         <Search />
-        <NumResults />
+        <NumResults movies={movies} />
         
       </nav>
 }
@@ -91,21 +91,21 @@ function Search(){
         />
 }
 
-function NumResults(){
+function NumResults({movies}){
   return <p className="num-results">
-          Found <strong>X</strong> results
+          Found <strong>{movies.length}</strong> results
         </p>
 }
 
 
-function Main(){
+function Main({movies}){
 
   
 
 
   return <main className="main">
 
-    <ListBox />
+    <ListBox movies={movies} />
     <WatchedBox />
         
 
@@ -114,7 +114,7 @@ function Main(){
 }
 
 
-function ListBox(){
+function ListBox({movies}){
 
   
 
@@ -128,14 +128,14 @@ function ListBox(){
             {isOpen1 ? "–" : "+"}
           </button>
           {isOpen1 && (
-            <MovieList />
+            <MovieList movies={movies} />
           )}
         </div>
 }
 
-function MovieList(){
+function MovieList({movies}){
 
-  const [movies, setMovies] = useState(tempMovieData);
+  
 
   return <ul className="list">
               {movies?.map((movie) => (
